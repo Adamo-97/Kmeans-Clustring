e3c0ef80ed9603c8acf46b1eb09848075bc8e127
tr3sp4ss3rexe <xd3nux@protonmail.com>
2025-10-03 13:16:07 +0100

    Yes UwU

--- main.c
+++ main.c
@@ -2,22 +2,27 @@
 #include <stdlib.h>
 
 void readData(char* filename);
-void freeData();
 void kmeansImplementation(int k);
 
 int main(int argc, char** argv) {
 
     if (argc != 3) {
-        printf("Error! Usage: ./kmeans <data-file-name> <number-of-centroids>");
+        printf("Error! Usage: ./kmeans <data-file-name> <number-of-clusters>");
         exit(0);
     }
+
+    // char choice;
+
+    // printf("Would you like to assign the initial centroid positions? (Y/N)")
+    // scanf("%c", choice);
+
+    // if (choice == "Y" || choice == "y") {
+    //     printf("")
+    // }
 
     int k = atoi(argv[2]);
 
     readData(argv[1]);
-
     kmeansImplementation(k);
-
-    freeData();
     return 0;
 }
